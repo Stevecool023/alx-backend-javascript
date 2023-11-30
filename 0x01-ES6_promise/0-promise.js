e05510f0498f94849815c69baa7394a255f390b8
Stevecool023 <tallmankaranjastevey@gmail.com>
2023-11-29 17:59:02 -0800

    promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,10 @@
 export default function getResponseFromAPI() {
-  return (new Promise((resolve, reject) => {}));
+  return new Promise((resolve, reject) => {
+    // Simulating an asynchronous operation (e.g., fetching data from an API)
+    setTimeout(() => {
+      // For demonstration purposes, let's resolve the promise with a dummy response
+      const response = { data: "Dummy data from API" };
+      resolve(response);
+    }, 1000); // Simulating a delay of 1 second
+  });
 }
