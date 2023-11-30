a2d640bf72602d178fb663195f767b83c1719fa1
Stevecool023 <tallmankaranjastevey@gmail.com>
2023-11-29 18:14:30 -0800

    promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,16 +1,3 @@
-// 0-promise.js
-
-function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // Your asynchronous API call or other asynchronous operations go here
-    // For example:
-    setTimeout(() => {
-      const data = "Response from the API"; // Replace this with your actual API response
-      resolve(data);
-      // or if there's an error:
-      // reject(new Error("Failed to get response from the API"));
-    }, 1000); // Simulating an asynchronous operation with setTimeout
-  });
+export default function getResponseFromAPI() {
+  return new Promise(() => {});
 }
-
-export default getResponseFromAPI;
