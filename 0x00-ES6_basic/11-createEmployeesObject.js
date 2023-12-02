9ef968093138eaa7784de959fe27ed7bf4cfac88
Stevecool023 <karanjastephenn@gmail.com>
2023-12-02 23:26:08 +0300

    Iterator

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,4 +1,4 @@
-export default function createEmployeesObject(departmentName, employees) {
+export default function createEmployeeObject(departmentName, employees) {
   return {
     [departmentName]: [
       ...employees,
